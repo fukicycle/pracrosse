d1154626e8b609957ec9059782dfe85cb9e6a641
fukicycle <lego.sato.4135@gmail.com>
2024-06-27 13:19:08 +0900

    add pwa updator

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using pracrosse;
 using Blazored.LocalStorage;
-using BlazorAnimate;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddPWAUpdater();
 builder.Services.AddScoped<SettingService>();
 await builder.Build().RunAsync();
